564f0d4861248bff5111da9ab8700c65e50a0eb5
usmaniraghib <javaprogrammers786@gmail.com>
2023-09-06 16:41:14 +0530

    Converted into maven project.

--- src/test/java/com/raghib/selenium/SeleniumExample.java
+++ src/test/java/com/raghib/selenium/SeleniumExample.java
@@ -5,15 +5,15 @@ package com.raghib.selenium;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+//TODO - TestNG and Cucumber code implementation is pending.
 public class SeleniumExample {
 
 	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "\\Driver-116\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
         driver.get("https://www.google.com");
         String url = driver.getCurrentUrl();
         System.out.println("URL : "+url);
         driver.quit();
-
 	}
-
 }
--- src/test/resources/com/raghib/selenium/JavaExample.java
+++ src/test/resources/com/raghib/selenium/JavaExample.java
@@ -5,5 +5,4 @@ public class JavaExample {
 	public static void main(String[] args) {
 		System.out.println("Hello Google");
 	}
-
 }
